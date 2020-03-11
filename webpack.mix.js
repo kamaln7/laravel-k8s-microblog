905371231ccf0dafa842c6131d45c5a3c0e1df21
Kamal Nasser <hello@kamal.io>
2020-03-11 15:59:49 +0200

    docker image laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.setPublicPath('public')
+    .js('resources/js/app.js', 'public/js')
     .styles([
         'resources/css/tachyons.min.css'
     ], 'public/css/app.css');
