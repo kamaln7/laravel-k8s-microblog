cc98a1c589ef1e22545caa615a6079d21fe95b04
Kamal Nasser <hello@kamal.io>
2020-03-12 17:27:52 +0200

    actually schedule RemoveStalePosts

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,8 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
+use \App\Jobs\RemoveStalePosts;
+
 class Kernel extends ConsoleKernel
 {
     /**
@@ -25,6 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('horizon:snapshot')->everyFiveMinutes();
+        $schedule->job(new RemoveStalePosts)->everyFifteenMinutes();
     }
 
     /**
